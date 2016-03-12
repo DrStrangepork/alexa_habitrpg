b216c5e96af260b06db0f2a979474d32e9bf3f74
Matthew Zygowicz <matt@zygowicz.com>
2016-03-12 10:22:46 -0600

    Added files ot the GIT IGNORE.  Completely setup GruntTasks for lambda deployment.  Started the readme

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+require('dotenv').load();
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+   lambda_invoke: {
+      default: {
+         options: {
+            file_name: 'index.js'
+         }
+      }
+   },
+   lambda_deploy: {
+      default: {
+         package: 'HabitRPGAlexaSkill',
+         arn: process.env.AMAZON_LAMBDA_ARN
+      }
+   },
+   lambda_package: {
+      default: {
+         package: 'HabitRPGAlexaSkill'
+      }
+   }
+});
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
